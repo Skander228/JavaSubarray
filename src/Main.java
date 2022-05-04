3a85f5360293ecad518eab9021aac87e746efb15
Skander228 <kkhemiriskander@gmail.com>
2022-05-04 21:50:45 +0300

    New

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int sum = 0, c = 0;
+        int sum, c = 0;
         int amount = sc.nextInt();
         int[] numArray = new int[amount];
         for (int i = 0; i < amount; i++) {
             numArray[i] = sc.nextInt();
         }
         sc.close();
-        for(int i = 0; i < amount; i++) {
+        for (int i = 0; i < amount; i++) {
             System.out.println(i);
-            for(int j = i; j < amount; j++) {
+            for (int j = i; j < amount; j++) {
                 sum = 0;
-                for(int k=i; k <= j; k++) {
+                for (int k=i; k <= j; k++) {
                     sum += numArray[k];
                 }
-                if(sum < 0) {
+                if (sum < 0) {
                     c++;
                 }
             }
